d0d2829bd4407e3da6ff3bc33c16299decf06e8a
Aaron Saw Min Sern <aaron.sawms@shopee.com>
2022-05-13 19:06:05 +0800

    format config.lua

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -47,6 +47,7 @@ vim.g.copilot_no_tab_map = true
 vim.cmd([[imap <silent><script><expr> <C-A-n> copilot#Accept("\<CR>")]])
 
 lvim.keys.normal_mode["tt"] = { "<cmd>TroubleToggle<cr>", { noremap = true } }
+lvim.keys.normal_mode["tr"] = { "<cmd>setlocal readonly!<cr>", { noremap = true } }
 lvim.keys.normal_mode["ts"] = { "<cmd>setlocal spell!<cr>", { noremap = true } }
 lvim.keys.normal_mode["tl"] = { "<cmd>setlocal list!<cr>", { noremap = true } }
 lvim.keys.normal_mode["tg"] = { "<cmd>Goyo<cr>", { noremap = true } }
@@ -65,48 +66,48 @@ lvim.keys.normal_mode["<Space>0"] = { "<cmd>lua require('harpoon.ui').nav_next()
 
 -- autocommands
 lvim.autocommands.text_files = {
-	{ "BufNewFile,BufFilePre,BufRead", "*.md", "setlocal filetype=markdown.pandoc" },
-	{
-		"BufNewFile,BufFilePre,BufRead",
-		"*.md,*.tex,*.txt",
-		"setlocal textwidth=79 | setlocal spell",
-	},
+  { "BufNewFile,BufFilePre,BufRead", "*.md", "setlocal filetype=markdown.pandoc" },
+  {
+    "BufNewFile,BufFilePre,BufRead",
+    "*.md,*.tex,*.txt",
+    "setlocal textwidth=79 | setlocal spell",
+  },
 }
 
 -- core plugins
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = { "<cmd>Telescope grep_string<CR>", "Grep string" }
 lvim.builtin.which_key.mappings["T"] = {
-	name = "+Trouble",
-	r = { "<cmd>Trouble lsp_references<cr>", "References" },
-	f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-	d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-	q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-	w = { "<cmd>Trouble workspace_diagnostics<cr>", "Diagnostics" },
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Diagnostics" },
 }
 
 lvim.builtin.treesitter.ensure_installed = "all"
 lvim.builtin.cmp.enabled = function()
-	return vim.g.cmp_toggle_flag
+  return vim.g.cmp_toggle_flag
 end
 
 local _, actions = pcall(require, "telescope.actions")
 require("telescope").load_extension("harpoon")
 lvim.builtin.telescope.defaults.mappings = {
-	-- for input mode
-	i = {
-		["<C-j>"] = actions.move_selection_next,
-		["<C-k>"] = actions.move_selection_previous,
-		["<C-n>"] = actions.cycle_history_next,
-		["<C-p>"] = actions.cycle_history_prev,
-	},
-	-- -- for normal mode
-	n = {
-		["<C-j>"] = actions.move_selection_next,
-		["<C-k>"] = actions.move_selection_previous,
-		["<C-c>"] = actions.close,
-	},
+  -- for input mode
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  -- -- for normal mode
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-c>"] = actions.close,
+  },
 }
 
 lvim.builtin.notify.active = true
@@ -134,53 +135,54 @@ lvim.lsp.automatic_servers_installation = true
 -- set a formatter
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-	{ command = "black", filetypes = { "python" } },
-	{ command = "isort", filetypes = { "python" } },
-	{ command = "stylua", filetypes = { "lua" } },
-	{
-		-- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-		command = "prettier",
-		filetypes = { "typescript", "vue" },
-		-- extra_args = { "--print-with", "100" },
-	},
+  { command = "black", filetypes = { "python" } },
+  { command = "isort", filetypes = { "python" } },
+  { command = "stylua", filetypes = { "lua" } },
+  { command = "gofmt", filetypes = { "go" } },
+  {
+    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+    command = "prettier",
+    filetypes = { "typescript", "vue" },
+    -- extra_args = { "--print-with", "100" },
+  },
 })
 
 -- set linters
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
-	{ command = "flake8", filetypes = { "python" } },
-	{ command = "eslint", filetypes = { "typescript" } },
-	{
-		command = "shellcheck",
-		-- extra_args = { "--severity", "warning" },
-	},
+  { command = "flake8", filetypes = { "python" } },
+  { command = "eslint", filetypes = { "typescript" } },
+  {
+    command = "shellcheck",
+    -- extra_args = { "--severity", "warning" },
+  },
 })
 
 -- additional plugins
 lvim.plugins = {
-	{ "folke/tokyonight.nvim" },
-	{ "cocopon/iceberg.vim" },
-	{ "shaunsingh/nord.nvim" },
-	{ "ellisonleao/gruvbox.nvim" },
+  { "folke/tokyonight.nvim" },
+  { "cocopon/iceberg.vim" },
+  { "shaunsingh/nord.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
 
-	{ "theprimeagen/harpoon" },
-	{ "norcalli/nvim-colorizer.lua" },
-	{ "folke/trouble.nvim" },
-	{ "junegunn/goyo.vim" },
+  { "theprimeagen/harpoon" },
+  { "norcalli/nvim-colorizer.lua" },
+  { "folke/trouble.nvim" },
+  { "junegunn/goyo.vim" },
 
-	{ "vim-pandoc/vim-pandoc-syntax" },
-	{
-		"lervag/vimtex",
-		config = function()
-			vim.g.tex_flavor = "latex"
-			vim.g.vimtex_view_method = "zathura"
-			vim.g.vimtex_quickfix_mode = 0
-			vim.g.tex_conceal = "abdmg"
-			vim.cmd("hi Conceal guibg=none ctermbg=none")
-		end,
-		ft = "tex",
-	},
-	{ "vlime/vlime",
+  { "vim-pandoc/vim-pandoc-syntax" },
+  {
+    "lervag/vimtex",
+    config = function()
+      vim.g.tex_flavor = "latex"
+      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_quickfix_mode = 0
+      vim.g.tex_conceal = "abdmg"
+      vim.cmd("hi Conceal guibg=none ctermbg=none")
+    end,
+    ft = "tex",
+  },
+  { "vlime/vlime",
     rtp = "vim/"
   },
   { "github/copilot.vim" }
